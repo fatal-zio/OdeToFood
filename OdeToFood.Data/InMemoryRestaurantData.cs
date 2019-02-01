76c05bce91b11d907faeb7b748620af14d37724c
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-02-01 10:16:55 -0600

    Added basic Razor page to display Restaurant List.

---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private readonly List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 1, Name = "Joe's Mexican Restaurant", Location = "Texas", Cuisine = CuisineType.Mexican },
+                new Restaurant { Id = 1, Name = "Jeffrey's Lounge", Location = "Manitoba", Cuisine = CuisineType.Comfort },
+                new Restaurant { Id = 1, Name = "Clay Oven", Location = "Manitoba", Cuisine = CuisineType.Indian }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in _restaurants
+                orderby r.Name
+                select r;
+        }
+    }
+}
